95ae6786681f12c4004c21c754f581e19c4ee88c
nurcin <nurchin.14@gmail.com>
2024-12-01 03:12:29 +0400

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarTask
+{
+  
+    public class Car
+    {
+        private static int _idCounter=1;
+
+        public int Id { get; }
+        public string Model { get; set; }
+        public string Brand { get; set; }
+        public CarType Type { get; set; }
+
+        public enum CarType
+        {
+            Sport,
+            SUV, 
+            Sedan
+        }
+
+        public Car(string brand, string model, CarType type)
+        {
+            Id = _idCounter++;
+            Brand = brand;
+            Model = model;
+            Type = type;    
+
+        }
+        public override string ToString()
+        {
+            return $"Car Id:{Id},Brand:{Brand}, Model:{Model}, Type:{Type}";
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+namespace CarTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car car1 = new Car("Mercedes","E200",Car.CarType.Sedan);
+            Car car2 = new Car("Tesla","Cybertruck",Car.CarType.SUV);
+
+            Console.WriteLine(car1);
+            Console.WriteLine(car2);
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
